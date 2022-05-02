391e952667735e10e4ac2699e37cfdfd80da8634
Petru (Desktop, Dell XPS 8930) <petru.tarabuta@gmail.com>
2022-05-02 20:25:25 +0100

    1. Added a C# project to store utility methods (ProjectEulerUtilities.csproj). 2. Solved Problem 1

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ProjectEulerUtilities;
+using System.Diagnostics;
+
+Problem1();
+
+static void Problem1()
+{
+    Stopwatch executionTimer = Stopwatch.StartNew();
+    int sum = 0;
+    for (int i = 1; i <= 1000; i++)
+    {
+        if (NumericUtils.IsXDivisibleByY(i,3))
+        {
+            sum += i;
+        }
+        else if (NumericUtils.IsXDivisibleByY(i, 5))
+        {
+            sum += i;
+        }
+    }
+    Console.WriteLine($"Answer = {sum}");
+    Console.WriteLine($"Execution time = {executionTimer.ElapsedMilliseconds} ms");
+}
+
+
+
+
+// Problem template
+static void ProblemN()
+{
+    Stopwatch executionTimer = Stopwatch.StartNew();
+    int answer = 0;
+    
+    // 
+    // add code here
+    //
+    
+    Console.WriteLine($"Answer = {answer}");
+    Console.WriteLine($"Execution time = {executionTimer.ElapsedMilliseconds} ms");
+}
---
+++ ProjectEulerUtilities/NumericUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ProjectEulerUtilities
+{
+    public static class NumericUtils
+    {
+        #region Static Methods
+        public static bool IsXDivisibleByY(int x, int y)
+        {
+            if (x%y == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        #endregion
+
+    }
+}
